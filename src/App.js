fa316a4be149d31e1586bb50f9bc0cc28359c20c
Precious Ihire <preciousihire@gmail.com>
2022-10-12 17:19:29 +0100

    Altschool Calculator App

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import { useState } from 'react';
+
+function App() {
+	const [calc, setCalc] = useState("");
+	const [result, setResult] = useState("");
+
+	const ops = ['/', '*', '+', '-', '.'];
+
+	const updateCalc = value => {
+
+		if (
+			// eslint-disable-next-line no-mixed-operators
+			ops.includes(value) && calc === '' ||
+            // eslint-disable-next-line no-mixed-operators
+            ops.includes(value) && ops.includes(calc.slice(-1)
+			)
+		) {
+			return;
+		}
+
+		setCalc(calc + value);
+
+		if (!ops.includes(value)) {
+			// eslint-disable-next-line no-eval
+			setResult(eval(calc + value).toString());
+		}
+	}
+
+	const createDigits = () => {
+		const digits = [];
+
+		for (let i = 1; i < 10; i++) {
+			digits.push(
+				<button 
+				    onClick={() => updateCalc(i.toString())} 
+				    key={i}>
+				    {i}
+				</button>
+			)
+		}
+
+		return digits;
+	} 
+
+	const calculate = () => {
+		// eslint-disable-next-line no-eval
+		setCalc(eval(calc).toString());
+	}
+
+	const deleteLast = () => {
+		if (calc === '') {
+			return;
+		}
+
+		const value = calc.slice(0, -1);
+
+		setCalc(value);
+	}
+
+	return (
+		<div className="App">
+			<div className="calculator">
+				<div className="display">
+					{result ? <span>({result})</span> : ''}
+					&nbsp;
+					{calc || "0" }
+				</div>
+
+			<div className="operators">
+				<button onClick={ () => updateCalc('/')}>/</button>
+				<button onClick={ () => updateCalc('*')}>*</button>
+				<button onClick={ () => updateCalc('+')}>+</button>
+				<button onClick={ () => updateCalc('-')}>-</button>
+
+				<button onClick={deleteLast}>DEL</button>
+			</div>
+
+			<div className="digits">
+				{ createDigits() }
+				<button onClick={ () => updateCalc('0')}>0</button>
+				<button onClick={ () => updateCalc('.')}>.</button>
+
+				<button onClick={calculate}>=</button>
+			</div>	
+			</div>
+		</div>
+	);
+}
+
+export default App;
